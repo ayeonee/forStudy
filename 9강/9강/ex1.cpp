80deb6939cb27972f2f303357f76bebc6764e4fb
ayeonee <zn_yeon@naver.com>
2020-07-13 18:59:21 +0900

    test

---
+++ 9강/9강/ex1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cout << "두 숫자를 입력하세요." << endl;
+	cin >> a >> b;
+	cout << "두 수의 합은 " << a + b << endl;
+	return 0;
+}
---
+++ 9강/9강/ex3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	printf("%c %c\n", a,a + 1);
+	return 0;
+}
