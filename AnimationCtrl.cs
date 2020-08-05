3dfe64662eb623d0c5a7990926c3fd3244384e7d
ayeonee <68219942+ayeonee@users.noreply.github.com>
2020-08-05 23:19:01 +0900

    day3
    my first unity project cs = animation ctrl

---
+++ AnimationCtrl.cs
@@ -0,0 +1,46 @@
+﻿using CoherentNoise.Generation.Displacement;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class AnimationCtrl : MonoBehaviour
+{
+    public float speed = 20;
+    protected Animator mAvatar;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        mAvatar = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            mAvatar.SetTrigger("ATTACK");
+        }     
+        
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            mAvatar.SetTrigger("MOVE");
+        }
+
+        if (Input.GetKeyUp(KeyCode.S)){
+            mAvatar.SetTrigger("RUN");
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+             transform.Rotate(UnityEngine.Vector3.up * 180);
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            transform.Rotate(UnityEngine.Vector3.up * 90);
+        }
+   
+    }
+}
