8d78a6388908911ba1dac8c9aeb92f49162e1e5e
ayeonee <68219942+ayeonee@users.noreply.github.com>
2020-08-06 15:23:51 +0900

    day4
    action project

--- AnimationCtrl.cs
+++ AnimationCtrl.cs
@@ -2,37 +2,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class AnimationCtrl : MonoBehaviour
 {
-    public float speed = 20;
+    
     protected Animator mAvatar;
     // Start is called before the first frame update
+    int n = 0;
 
     void Start()
     {
         mAvatar = GetComponent<Animator>();
     }
+    private void OnCollisionStay(Collision collision)
+    {
+        GameObject womanPlayer = GameObject.Find("Female A");
+        AnimationCtrlW playerScript = womanPlayer.GetComponent<AnimationCtrlW>();
+        if (playerScript.flag == 1)
+        {
+            mAvatar.SetTrigger("BLOCK");
+        }
 
-    // Update is called once per frame
+        if (playerScript.flag == 2)
+        {
+            if (n == 0)
+            {
+                mAvatar.SetTrigger("DIE");
+                n = 1;
+            }
+        }
+    }
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            mAvatar.SetTrigger("ATTACK");
-        }     
         
+         if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            mAvatar.SetTrigger("ATTACK1");
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            mAvatar.SetTrigger("ATTACK2");
+        }
+
+
         if (Input.GetKeyUp(KeyCode.F))
         {
             mAvatar.SetTrigger("MOVE");
         }
 
-        if (Input.GetKeyUp(KeyCode.S)){
+        if (Input.GetKeyUp(KeyCode.D)){
             mAvatar.SetTrigger("RUN");
         }
 
-        if (Input.GetKeyUp(KeyCode.D))
+        if (Input.GetKeyUp(KeyCode.S))
         {
              transform.Rotate(UnityEngine.Vector3.up * 180);
         }
---
+++ AnimationCtrlW.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCtrlW : MonoBehaviour
+{
+    public int flag = 0;
+    protected Animator mAvatar;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        mAvatar = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("woman");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            mAvatar.SetTrigger("ATTACK1");
+            flag = 1;
+           StartCoroutine(WaitForIt());
+        }
+
+        if (Input.GetKeyDown(KeyCode.End))
+        {
+            mAvatar.SetTrigger("ATTACK2");
+           flag = 2;
+            StartCoroutine(WaitForIt());
+        }
+
+
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            mAvatar.SetTrigger("MOVE");
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            mAvatar.SetTrigger("RUN");
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            transform.Rotate(UnityEngine.Vector3.up * 180);
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            transform.Rotate(UnityEngine.Vector3.up * 90);
+        }
+
+    }
+
+    IEnumerator WaitForIt()
+    {
+        yield return new WaitForSeconds(0.1f);
+        flag = 0;
+    }
+}
+
