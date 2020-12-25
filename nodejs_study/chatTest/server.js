ff0da5f0477124873444f1116def535845c94405
ayeonee <zn_yeon@naver.com>
2020-12-25 16:49:40 +0900

    study chat2

--- nodejs_study/chatTest/server.js
+++ nodejs_study/chatTest/server.js
@@ -20,15 +20,19 @@ io.on('connection', function(socket){
     var name="익명"+count++;    //처음 접속햇을 때 익명1, 익명2..
     socket.name=name;           //닉네임이 변경될 때 비교해주기 위한 값, 현재 닉네임을 넣어줌
     io.to(socket.id).emit('create name', name); //서버가 해당 socket id에만 이벤트를 전달. 채팅방에 접속한 유저마다 닉네임이 다르기 때문에 각 유저에게만 닉네임이 보여주기 위해 create name이라는 이벤트 발생시켜줌.
+    io.emit('new_connect',name);
 
     //채팅 접속이 끊어졌을 때 2
     socket.on('disconnect', function(){
         console.log('user disconnected: '+socket.id+' '+socket.name);
+        io.emit('new_disconnect', socket.name);
     });
 
     //메세지를 보냈을 때 3
     socket.on('send message', function(name, text){
         var msg=name+' : '+text;
+        if(name!=socket.name)
+            io.emit('change name',socket.name, name);
         socket.name=name;
         console.log(msg);
         io.emit('receive message', msg);
