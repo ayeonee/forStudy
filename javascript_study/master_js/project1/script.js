e5f67fcd003a17f3fb38f54277f6f774da035472
ayeonee <zn_yeon@naver.com>
2021-01-06 15:58:04 +0900

    push data in table & mk btn

--- javascript_study/master_js/project1/script.js
+++ javascript_study/master_js/project1/script.js
@@ -35,12 +35,94 @@ var crudApp = new function(){
             tr.appendChild(th);
         }
 
+        //td작성
+        for(var i=0; i<this.myClass.length; i++){
+            //table에 한 행 추가
+            tr=table.insertRow(-1);
+            //table header 길이만큼 순회하여 거기에 매칭되는 데이터 갖고오기
+            for(var j=0; j<this.col.length; j++){
+                var tabCell=tr.insertCell(-1);
+                tabCell.innerHTML=this.myClass[i][this.col[j]];
+            }
+
+            //update 버튼 만들기
+            this.td=document.createElement('td');   //td= table data
+            tr.appendChild(this.td);
+            var btnUpdate=document.createElement('input');
+            btnUpdate.setAttribute('type', 'button');
+            btnUpdate.setAttribute('value', 'Update');
+            btnUpdate.setAttribute('id', 'Edit'+i); //id는 유일해야하기때문
+            btnUpdate.setAttribute('style', 'background-color:#44CCEB');
+            btnUpdate.setAttribute('onClick', 'crudApp.Update(this)');      //버튼 클릭될 때 실행할 메서드
+            this.td.appendChild(btnUpdate);
+
+            //save 버튼 만들기
+            tr.appendChild(this.td);
+            var btnSave=document.createElement('input');
+            btnSave.setAttribute('type', 'button');
+            btnSave.setAttribute('value', 'Save');
+            btnSave.setAttribute('id', 'Save'+i);
+            btnSave.setAttribute('style', 'display:none;');
+            btnSave.setAttribute('onClick', 'crudApp.Save(this)');
+            this.td.appendChild(btnSave);
+
+            //Delete 버튼 만들기
+            this.td=document.createElement('td');  
+            tr.appendChild(this.td);
+            var btnDelete=document.createElement('input');
+            btnDelete.setAttribute('type', 'button');
+            btnDelete.setAttribute('value', 'Delete');
+            btnDelete.setAttribute('id', 'Delete'+i);
+            btnDelete.setAttribute('style', 'background-color:#ED5650;');
+            btnDelete.setAttribute('onClick', 'crudApp.Delete(this)');
+            this.td.appendChild(btnDelete);
+        }
+
+        //입력행 추가
+        tr=table.insertRow(-1);
+        for(var j=0; j<this.col.length; j++){
+            var newCell=tr.insertCell(-1);
+            if(j>=1){
+                if(j==2){
+                    //선택 항목 만들기
+                    var select=document.createElement('select');
+                    select.innerHTML='<option value=""></option>';
+                    for(var k=0; k<this.Category.length; k++){
+                        select.innerHTML= select.innerHTML + `<option value="${this.Category[k]}">${this.Category[k]}</option>`;
+                    }
+                    newCell.appendChild(select);
+                }
+                else{
+                    var tBox=document.createElement('input');
+                    tBox.setAttribute('type', 'text');
+                    tBox.setAttribute('value', '');
+                    newCell.appendChild(tBox);
+                }
+            }
+        }
+
+        //create 버튼 만들기
+        this.td=document.createElement('td');  
+        tr.appendChild(this.td);
+        var btnCreate=document.createElement('input');
+        btnCreate.setAttribute('type', 'button');
+        btnCreate.setAttribute('value', 'Create');
+        btnCreate.setAttribute('id', 'New'+i);
+        btnCreate.setAttribute('style', 'background-color:#207DD1;');
+        btnCreate.setAttribute('onClick', 'crudApp.CreateNew(this)');
+        this.td.appendChild(btnCreate);
+
         var div=document.getElementById('container');
         div.innerHTML='수강관리 앱';
         div.appendChild(table);
     }
-        
-
+    
+    //삭제 메서드(눌린 버튼을 인자로 받아 해당 버튼에 해당하는 행 삭제하기)
+    this.Delete= (oButton)=>{
+        var targetIdx=oButton.parentNode.parentNode.rowIndex;   //input 태그 감싸고 있는 td, td 감싸고 있는 tr 가리킴. 그 tr의 rowIndex 뽑아냄.
+        this.myClass.splice((targetIdx-1),1);
+        this.createTable();     //table 새로고침
+    }
 }
 
 crudApp.createTable();
