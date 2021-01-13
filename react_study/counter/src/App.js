0ff356269e1f2b0c87c43647292789319e509b7f
ayeonee <zn_yeon@naver.com>
2021-01-13 23:38:48 +0900

    study LifeCycle API

--- react_study/counter/src/App.js
+++ react_study/counter/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CountButton from "./components/CountButton";
 import Number from "./components/Number";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   margin : 0 auto;
@@ -22,6 +22,28 @@ const ButtonWrapper =styled.div`
 class App extends Component{
   state = { number : 0} //state 선언
 
+  //컴포넌트 마운트 되기 전에 호출
+  constructor(props){
+    super(props);
+    console.log("constructor 호출");
+  }
+
+  //컴포넌트 마운트 된 직후 호출
+  componentDidMount(){
+    console.log("componentDidMount 호출");
+  }
+
+  //props 나 state가 갱신되어 렌더링이 발생하기 직전에 호출
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("shouldComponentUpdate 호출");
+    if(nextState.number % 3===0) return false;
+    return true;
+  }
+
+  //render 메소드 호출된 뒤에 발생.
+  componentDidUpdate(prevProps, prevState){
+    console.log("componentDidUpdate 호출");
+  }
   // 1. state를 업데이트하는 함수 전달
   countUP=()=>{
     //setState : 전달되는 값을 업데이트해주는 함수.
@@ -40,6 +62,8 @@ class App extends Component{
 
   render(){
     const {number} = this.state;
+
+    console.log("render 호출");
     return (
       <Wrapper>
         <ButtonWrapper>
